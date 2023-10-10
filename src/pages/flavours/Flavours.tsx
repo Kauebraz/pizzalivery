356c5caf0cb3e50db6d30c7633d115c3dc1b5ee1
Kauebraz <rm97768@fiap.com.br>
2023-10-10 20:24:24 -0300

    Selecionando 2 sabores de pizza

--- src/pages/flavours/Flavours.tsx
+++ src/pages/flavours/Flavours.tsx
@@ -1,16 +1,16 @@
-import { useContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { Button } from "../../components/button/Button"
-import { Layout } from "../../components/layout/Layout"
-import { routes } from "../../routes"
-import OrderContext from "../../contexts/OrderContext"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../../components/button/Button";
+import { Layout } from "../../components/layout/Layout";
+import { routes } from "../../routes";
+import OrderContext from "../../contexts/OrderContext";
 
-import Mussarela from "../../assets/pizza-flavours/mucarela.png"
-import ChickenWithCheese from "../../assets/pizza-flavours/frango-catupiry.png"
-import Margherita from "../../assets/pizza-flavours/margherita.png"
-import Lusa from "../../assets/pizza-flavours/portuguesa.png"
+import Mussarela from "../../assets/pizza-flavours/mucarela.png";
+import ChickenWithCheese from "../../assets/pizza-flavours/frango-catupiry.png";
+import Margherita from "../../assets/pizza-flavours/margherita.png";
+import Lusa from "../../assets/pizza-flavours/portuguesa.png";
 
-import { convertToCurrency } from "../../helpers/convertToCurrency"
+import { convertToCurrency } from "../../helpers/convertToCurrency";
 
 import {
   FlavourActionWrapper,
@@ -20,13 +20,13 @@ import {
   FlavourCardPrice,
   FlavourCardTitle,
   FlavourContentWrapper,
-} from "./Flavours.styles"
-import { Title } from "../../components/title/Title"
+} from "./Flavours.styles";
+import { Title } from "../../components/title/Title";
 
 export default function Flavours() {
-  const navigate = useNavigate()
-  const { pizzaSize, pizzaFlavour, setPizzaFlavour } = useContext(OrderContext)
-  const [flavourId, setflavourId] = useState("")
+  const navigate = useNavigate();
+  const { pizzaSize, pizzaFlavour, setPizzaFlavour } = useContext(OrderContext);
+  const [selectedFlavours, setSelectedFlavours] = useState([]);
 
   const flavoursOptions = [
     {
@@ -77,38 +77,52 @@ export default function Flavours() {
         "1": 23.5,
       },
     },
-  ]
+  ];
 
-  const getPizzaFlavour = (id: string) => {
-    return flavoursOptions.filter((flavour) => flavour.id === id)
-  }
+  const getPizzaFlavour = (id) => {
+    return flavoursOptions.find((flavour) => flavour.id === id);
+  };
 
-  const handleClick = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setflavourId(event.target.id)
-  }
+  const handleClick = (event) => {
+    const id = event.target.id;
+
+    if (selectedFlavours.includes(id)) {
+      setSelectedFlavours((prevSelectedFlavours) =>
+        prevSelectedFlavours.filter((flavourId) => flavourId !== id)
+      );
+    } else {
+      setSelectedFlavours((prevSelectedFlavours) => [...prevSelectedFlavours, id]);
+    }
+  };
 
   const handleBack = () => {
-    navigate(routes.pizzaSize)
-  }
+    navigate(routes.pizzaSize);
+  };
 
   const handleNext = () => {
-    const selectedFlavour = getPizzaFlavour(flavourId)
-    setPizzaFlavour(selectedFlavour)
-    navigate(routes.summary)
-  }
+    if (selectedFlavours.length == 2) {
+      const selectedFlavoursData = selectedFlavours.map((flavourId) =>
+        getPizzaFlavour(flavourId)
+      );
+      setPizzaFlavour(selectedFlavoursData);
+      navigate(routes.summary);
+    } else {
+      //
+    }
+  };
 
   useEffect(() => {
-    if (!pizzaFlavour) return
-
-    setflavourId(pizzaFlavour[0].id)
-  }, [])
+    if (pizzaFlavour && pizzaFlavour.length > 0) {
+      setSelectedFlavours(pizzaFlavour.map((flavour) => flavour.id));
+    }
+  }, [pizzaFlavour]);
 
   return (
     <Layout>
       <Title tabIndex={0}>Agora escolha o sabor da sua pizza</Title>
       <FlavourContentWrapper>
         {flavoursOptions.map(({ id, image, name, description, price }) => (
-          <FlavourCard key={id} selected={id === flavourId ? true : false}>
+          <FlavourCard key={id} selected={selectedFlavours.includes(id)}>
             <FlavourCardImage src={image} alt={name} />
             <FlavourCardTitle>{name}</FlavourCardTitle>
             <FlavourCardDescription>{description}</FlavourCardDescription>
@@ -125,8 +139,8 @@ export default function Flavours() {
         <Button inverse="inverse" onClick={handleBack}>
           Voltar
         </Button>
-        <Button onClick={handleNext}>Escolha o sabor</Button>
+        <Button onClick={handleNext}>Escolha os sabores</Button>
       </FlavourActionWrapper>
     </Layout>
-  )
+  );
 }
--- src/pages/summary/Summary.tsx
+++ src/pages/summary/Summary.tsx
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { Layout } from "../../components/layout/Layout"
-import { routes } from "../../routes"
-import OrderContext from "../../contexts/OrderContext"
-import { Title } from "../../components/title/Title"
-import { convertToCurrency } from "../../helpers/convertToCurrency"
+import { useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Layout } from "../../components/layout/Layout";
+import { routes } from "../../routes";
+import OrderContext from "../../contexts/OrderContext";
+import { Title } from "../../components/title/Title";
+import { convertToCurrency } from "../../helpers/convertToCurrency";
 import {
   SummaryActionWrapper,
   SummaryAmount,
@@ -14,69 +14,80 @@ import {
   SummaryImage,
   SummaryPrice,
   SummaryTitle,
-} from "./Summary.style"
-import { Button } from "../../components/button/Button"
+} from "./Summary.style";
+import { Button } from "../../components/button/Button";
 
 export default function Summary() {
-  const navigate = useNavigate()
-
-  const { pizzaSize, pizzaFlavour, setPizzaOrder } = useContext(OrderContext)
-  const [summaryData, setSummaryData] = useState({})
-  const [summaryAmount, setSummaryAmount] = useState(0)
+  const navigate = useNavigate();
+  const { pizzaSize, pizzaFlavour, setPizzaOrder } = useContext(OrderContext);
+  const [summaryData, setSummaryData] = useState([]);
+  const [summaryAmount, setSummaryAmount] = useState(0);
 
   const handleBack = () => {
-    navigate(routes.pizzaFlavour)
+    navigate(routes.pizzaFlavour);
   }
-  const handleNext = () => {
-    const payload = {
-      item: {
-        name: summaryData.name,
-        image: summaryData.image,
-        size: summaryData.text,
-        slices: summaryData.slices,
-        value: summaryData.price,
-      },
-      total: summaryAmount,
-    }
 
-    setPizzaOrder(payload)
-    navigate(routes.checkout)
+  const handleNext = () => {
+    // Aqui, você pode criar um payload para todos os sabores selecionados
+    const payload = pizzaFlavour.map((flavour) => ({
+      item: {
+        name: flavour.name,
+        image: flavour.image,
+        size: pizzaSize[0].text,
+        slices: pizzaSize[0].slices,
+        value: flavour.price[pizzaSize[0].slices],
+      },
+      total: flavour.price[pizzaSize[0].slices],
+    }));
+
+    setPizzaOrder(payload);
+    navigate(routes.checkout);
   }
 
   useEffect(() => {
     if (!pizzaFlavour) {
-      return navigate(routes.pizzaSize)
+      return navigate(routes.pizzaSize);
     }
 
     if (!pizzaSize) {
-      return navigate(routes.home)
+      return navigate(routes.home);
     }
 
-    setSummaryData({
+    const summaryData = pizzaFlavour.map((flavour) => ({
       text: pizzaSize[0].text,
       slices: pizzaSize[0].slices,
-      name: pizzaFlavour[0].name,
-      price: pizzaFlavour[0].price[pizzaSize[0].slices],
-      image: pizzaFlavour[0].image,
-    })
-  }, [])
+      name: flavour.name,
+      price: flavour.price[pizzaSize[0].slices],
+      image: flavour.image,
+    }));
+
+    setSummaryData(summaryData);
+  }, []);
 
   useEffect(() => {
-    setSummaryAmount(summaryData.price)
-  }, [summaryAmount])
+    // Calcule o valor total somando os preços de todos os sabores selecionados
+    const totalAmount = summaryData.reduce(
+      (total, flavour) => total + flavour.price,
+      0
+    );
+
+    setSummaryAmount(totalAmount);
+  }, [summaryData]);
 
   return (
     <Layout>
       <Title tabIndex={0}>Resumo do pedido</Title>
       <SummaryContentWrapper>
-        <SummaryDetails>
-          <SummaryImage src={summaryData.image} alt="" />
-          <SummaryTitle>{summaryData.name}</SummaryTitle>
-          <SummaryDescription>
-            {summaryData.text} {`(${summaryData.slices}) pedaços`}
-          </SummaryDescription>
-          <SummaryPrice>{convertToCurrency(summaryData.price)}</SummaryPrice>
-        </SummaryDetails>
+        {summaryData.map((item, index) => (
+          <SummaryDetails key={index}>
+            <SummaryImage src={item.image} alt="" />
+            <SummaryTitle>{item.name}</SummaryTitle>
+            <SummaryDescription>
+              {item.text} ({item.slices} pedaços)
+            </SummaryDescription>
+            <SummaryPrice>{convertToCurrency(item.price)}</SummaryPrice>
+          </SummaryDetails>
+        ))}
         <SummaryAmount>
           <SummaryPrice>{convertToCurrency(summaryAmount)}</SummaryPrice>
         </SummaryAmount>
@@ -88,5 +99,5 @@ export default function Summary() {
         <Button onClick={handleNext}>Ir para o pagamento</Button>
       </SummaryActionWrapper>
     </Layout>
-  )
+  );
 }
